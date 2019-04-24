c48980b1d35c4e598d5d59b4e503c1ccf5017e23
caniksea <caniksea@yahoo.co.nz>
2019-04-24 12:15:47 +0200

    Implemented repository for department

---
+++ src/main/java/com/caniksea/domain/Department.java
@@ -0,0 +1,6 @@
+package com.caniksea.domain;
+
+public class Department {
+
+    private String departmentId;
+}
---
+++ src/main/java/com/caniksea/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package com.caniksea.repository;
+
+import com.caniksea.domain.Department;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public interface DepartmentRepository extends  IRepository<Department, String> {
+
+}
